39952fcc646375dabafc8488328203938537d08b
Alexander Mokrushin <amokrushin@transportedlabs.com>
2023-04-13 19:16:02 +0300

    Added Lottie dep

--- Package.swift
+++ Package.swift
@@ -13,12 +13,15 @@ let package = Package(
 			targets: ["CUELiveWrapper", "CUEBluetooth", "engine", "TrueTime", "MQTTClient", "SocketRocket"]
 		),
 	],
-	dependencies: [],
+	dependencies: [
+        // Dependencies declare other packages that this package depends on.
+            .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "4.1.3")],
 	targets: [
 		.target(
 			name: "CUELiveWrapper",
 			dependencies: [
 				.target(name: "CUELive"),
+                "Lottie",
 			],
 			path: "CUELiveWrapper"
 		),
